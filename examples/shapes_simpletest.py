ee455d202459b2cdd6537ed7df54f17aa8538f64
ladyada <limor@ladyada.net>
2019-03-02 19:45:08 -0500

    draw some shapes why doncha

---
+++ examples/shapes_simpletest.py
@@ -0,0 +1,36 @@
+import displayio
+import board
+from adafruit_display_shapes.rect import Rect
+from adafruit_display_shapes.circle import Circle
+from adafruit_display_shapes.roundrect import RoundRect
+
+# Make the display context
+splash = displayio.Group(max_size=10)
+board.DISPLAY.show(splash)
+
+# Make a background color fill
+color_bitmap = displayio.Bitmap(320, 240, 1)
+color_palette = displayio.Palette(1)
+color_palette[0] = 0xFFFFFF
+bg_sprite = displayio.TileGrid(color_bitmap,
+                               pixel_shader=color_palette,
+                               position=(0, 0))
+print(bg_sprite.position)
+splash.append(bg_sprite)
+##########################################################################
+
+rect = Rect(80, 20, 41, 41, fill=0x0)
+splash.append(rect)
+
+circle = Circle(100, 100, 20, fill=0x00FF00, outline=0xFF00FF)
+splash.append(circle)
+
+rect2 = Rect(50, 100, 61, 81, outline=0x0, stroke=3)
+splash.append(rect2)
+
+roundrect = RoundRect(10, 10, 61, 81, 10, fill=0x0, outline=0xFF00FF, stroke=6)
+splash.append(roundrect)
+
+
+while True:
+    pass
