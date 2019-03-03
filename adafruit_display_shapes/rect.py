8d530c3f0c2b335318f337ca7696e9fae61e2aa1
ladyada <limor@ladyada.net>
2019-03-02 19:30:02 -0500

    rect outline thickness

--- adafruit_display_shapes/rect.py
+++ adafruit_display_shapes/rect.py
@@ -1,17 +1,19 @@
 import displayio
 
 class Rect(displayio.TileGrid):
-    def __init__(self, x, y, width, height, *, fill=None, outline=None):
+    def __init__(self, x, y, width, height, *, stroke=1, fill=None, outline=None):
         self._bitmap = displayio.Bitmap(width, height, 2)
         self._palette = displayio.Palette(2)
 
         if outline is not None:
             for w in range(width):
-                self._bitmap[w, 0] = 1
-                self._bitmap[w, height-1] = 1
+                for line in range(stroke):
+                    self._bitmap[w, line] = 1
+                    self._bitmap[w, height-1-line] = 1
             for h in range(height):
-                self._bitmap[0, h] = 1
-                self._bitmap[width-1, h] = 1
+                for line in range(stroke):
+                    self._bitmap[line, h] = 1
+                    self._bitmap[width-1-line, h] = 1
             self._palette[1] = outline
         if fill is not None:
             self._palette[0] = fill
